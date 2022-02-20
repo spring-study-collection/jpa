74261f473b7c33806767b797bb16a14940ae5d86
jungeun919 <renee8369@likelion.org>
2022-02-21 00:36:03 +0900

    1. 요구사항 분석과 기본 매핑

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        //EntityManager를 통해 DB 작업
+        EntityManager em = emf.createEntityManager();
+
+        //JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //DB에 SQL 쿼리를 보내고 커밋
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
